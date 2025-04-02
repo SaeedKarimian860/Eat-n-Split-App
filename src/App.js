7b1689cbb3b4b1768da1597dd8e751281f7e0a35
SaeedKarimian860 <SGK12190@gmail.com>
2025-04-02 19:40:45 -0400

    eat-n-split app

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ export default function App() {
       <div className="sidebar">
         <FriendsList />
         <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -79,6 +82,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💵 Bill Value</label>
+      <input type="text" />
+
+      <label>🧍🏻‍♂️ Your Expense</label>
+      <input type="text" />
+
+      <label>👩🏽‍🤝‍👨🏻 X's Expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
