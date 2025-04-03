41c5bf5e8fe452b113d6ee926942095eca077978
SaeedKarimian860 <SGK12190@gmail.com>
2025-04-02 21:29:37 -0400

    eat-n-split app

--- src/App.js
+++ src/App.js
@@ -87,13 +87,24 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
   return (
     <form className="form-add-friend">
       <label>👩🏽‍🤝‍👨🏻 Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🌄 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
